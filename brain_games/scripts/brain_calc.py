a571e0c0a31e4a62e3aeca28f4d81f881e779110
Vell1ngton <tema.mordvinov1@gmail.com>
2024-04-24 15:35:04 +0300

    STAGE 2

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3.
+import prompt
+import random
+
+
+def calc():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print('What is the result of the expression?')
+    count = 1
+    result = 0
+    while count != 4:
+        sign = ["+", "-", "*"]
+        random_number1 = random.randint(0, 20)
+        random_number2 = random.randint(0, 20)
+        random_sign = random.choice(sign)
+        if random_sign == '+':
+            result = random_number1 + random_number2
+        elif random_sign == '-':
+            result = random_number1 - random_number2
+        elif random_sign == '*':
+            result = random_number1 * random_number2
+        print(f'Question {random_number1} {random_sign} {random_number2}')
+        answer = prompt.string("Your answer: ")
+        if result == int(answer):
+            print('Correct!')
+            count += 1
+            continue
+        else:
+            return f"{answer} is wrong answer ;(. Correct answer was {result} \n Let's try again, {name}"
+    return f"Congratulations, {name}!"
+
+
+if __name__ == '__main__':
+    calc()
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,13 +3,14 @@
 import random
 import prompt
 
+
 def is_even():
     print('Welcome to the Brain Games!')
-    prompt.string('May I have your name? ')
+    name = prompt.string('May I have your name? ')
     print('Answer "yes" if the number is even, otherwise answer "no"')
     count = 1
     while count != 4:
-        random_number = random.randint(0,100)
+        random_number = random.randint(0, 100)
         print(f'Question {random_number}')
         answer = prompt.string("Your answer: ")
         if random_number % 2 != 0 and answer == 'no' or random_number % 2 == 0 and answer == 'yes':
@@ -18,11 +19,15 @@ def is_even():
             continue
         else:
             if random_number % 2 != 0 and answer == 'yes':
-                return "'yes' is wrong answer ;(. Correct answer was 'no'\n Let's try again, Bill"
+                return f"{answer} is wrong answer ;(. Correct answer was 'no'\n Let's try again, {name}'"
             if random_number % 2 == 0 and answer == 'no':
-                return "'no' is wrong answer ;(. Correct answer was 'yes'\n Let's try again, Bill"
-    return "Congratulations, Bill!"
+                return f"{answer} is wrong answer ;(. Correct answer was 'yes'\n Let's try again, {name}'"
+            if random_number % 2 != 0 and answer != 'yes' and answer != 'no':
+                return f"{answer} is wrong answer ;(. Correct answer was 'no'\n Let's try again, {name}'"
+            if random_number % 2 == 0 and answer != 'yes' and answer != 'no':
+                return f"{answer} is wrong answer ;(. Correct answer was 'yes'\n Let's try again, {name}'"
+    return f"Congratulations, {name}!"
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     is_even()
